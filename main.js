fcd2c3b4cfbd3d98a0c2b3b9e8136f4702c9a337
Fredrik Trobro <trobro@gmail.com>
2014-12-14 17:21:37 +0100

    auto spawn

--- main.js
+++ main.js
@@ -1,38 +1,58 @@
 var harvester = require('harvester');
 
-//for(var i in Game.spawns) {
-//    var mySpawn = Game.spawns[i];
-//    if (!mySpawn.spawning) {
-//        mySpawn.createCreep(body);
-//    }
-//}
-//
+var crp;
+function createCrp(typeName, count, maxCount, action) {
+    crp[typeName] = {
+        'typeName': typeName,
+        'count': count,
+        'maxCount': maxCount,
+        'action': action
+    };
+}
+
+function doAction(creep) {
+    myCrp = crp[creep.memory.role];
+    myCrp.count++;
+    myCrp.action(creep);
+}
+
+createCrp('harvester', 0, 3, function(creep) {
+    harvester(creep);
+});
+
+createCrp('builder', 0, 1, function(creep) {
+    if(creep.energy == 0) {
+        creep.moveTo(Game.spawns.Spawn1);
+        Game.spawns.Spawn1.transferEnergy(creep);
+    }
+    else {
+        var targets = creep.room.find(Game.CONSTRUCTION_SITES);
+        if(targets.length) {
+            creep.moveTo(targets[0]);
+            creep.build(targets[0]);
+        }
+    }
+});
+
+createCrp('guard', 0, 1, function(creep) {
+    var targets = creep.room.find(Game.HOSTILE_CREEPS);
+    if(targets.length) {
+        creep.moveTo(targets[0]);
+        creep.attack(targets[0]);
+    }
+});
+
 for(var name in Game.creeps) {
-	var creep = Game.creeps[name];
+    doAction(Game.creeps[name]);
+}
 
-	if(creep.memory.role == 'harvester') {
-		harvester(creep);
-	}
-
-	if(creep.memory.role == 'builder') {
-	
-		if(creep.energy == 0) {
-			creep.moveTo(Game.spawns.Spawn1);
-			Game.spawns.Spawn1.transferEnergy(creep);
-		}
-		else {
-			var targets = creep.room.find(Game.CONSTRUCTION_SITES);
-			if(targets.length) {
-				creep.moveTo(targets[0]);
-				creep.build(targets[0]);
-			}
-		}
-	}
-    if(creep.memory.role == 'guard') {
-    	var targets = creep.room.find(Game.HOSTILE_CREEPS);
-    	if(targets.length) {
-    		creep.moveTo(targets[0]);
-    		creep.attack(targets[0]);
-    	}
+for (var myCrp in crp) {
+    if (myCrp.count < myCrp.maxCount) {
+        for(var i in Game.spawns) {
+            var mySpawn = Game.spawns[i];
+            if (!mySpawn.spawning) {
+                mySpawn.createCreep(myCrp.body, null, {role: myCrp.typeName});
+            }
+        }
     }
 }
